8b157b90cce7b1b61d14cbc410e164075ec99974
AleksBobic <bobicaleks@yahoo.com>
2020-06-18 10:21:19 +0200

    Updates

---
+++ precache-manifest.10dc53e2f171be94b87b6caf54ec3be3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9b90eebef8cfc57149b1f85c2eb033",
+    "url": "/Phonebook-react-practice/index.html"
+  },
+  {
+    "revision": "8868d5d681232e039a8d",
+    "url": "/Phonebook-react-practice/static/css/main.2a4c4ab6.chunk.css"
+  },
+  {
+    "revision": "5e7fdd8d5c387b8bcef3",
+    "url": "/Phonebook-react-practice/static/js/2.084337d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Phonebook-react-practice/static/js/2.084337d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8868d5d681232e039a8d",
+    "url": "/Phonebook-react-practice/static/js/main.fa139f1f.chunk.js"
+  },
+  {
+    "revision": "0c815111fa3bcb064a9b",
+    "url": "/Phonebook-react-practice/static/js/runtime-main.99a2d480.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Phonebook-react-practice/precache-manifest.10dc53e2f171be94b87b6caf54ec3be3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Phonebook-react-practice/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
